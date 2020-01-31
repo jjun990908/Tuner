77c150d6b709d603cc4fdb1f486dd14a65c68ac9
kaspee <kaspee@naver.com>
2020-01-31 16:45:01 +0900

    FFT, Scale 함수 수정

--- app/src/main/java/com/cookandroid/tuner/ScaleSrc.java
+++ app/src/main/java/com/cookandroid/tuner/ScaleSrc.java
@@ -13,9 +13,14 @@ public class ScaleSrc {
     int do2_5, re2_5, mi2_5;
     SoundPool sp;
 
-    static int[] scaleArrayNormal;
-    static int[] scaleArrayFlat;
-    static int[] scaleArraySharp;
+    static double[] scaleArrayNormal;
+    static double[] scaleArrayFlat;
+    static double[] scaleArraySharp;
+    static {
+        scaleArrayNormal = new double[]{261.62, 293.66, 329.62, 349.22, 392, 440, 493.88, 523.25, 587.32, 659.25, 698.25, 784, 877.99, 987.76, 1046.5, 1174.6, 1318.51};
+        scaleArraySharp = new double[]{277.18, 311.12, 349.22, 370, 415.30, 466.16, 523.25, 554.36, 619.4, 698.5, 739, 830.60, 932.32, 1046.5, 1108.7, 1244.50, 1396.91};
+        scaleArrayFlat = new double[]{0, 277.18, 311.12, 329.62, 370, 415.30, 466.16, 493.88, 554.36, 619.4, 659.25, 739, 830.60, 932.32, 987.76, 1108.7, 1244.50};
+    }
 
     ScaleSrc(Context context) {
         this.sp = new SoundPool(5,         // 최대 음악파일의 개수
@@ -132,13 +137,6 @@ public class ScaleSrc {
         this.mi2_5 = sp.load(context,
                 R.raw.kal_mi2_5,
                 1);
-
-        scaleArrayNormal = new int[]{do0_0,re0_0,mi0_0,fa0_0,sol0_0,la0_0,si0_0,do1_0,
-                re1_0,mi1_0,fa1_0,sol1_0,la1_0,si1_0,do2_0,re2_0,mi2_0};
-        scaleArrayFlat = new int[]{do0_5,do0_5,re0_5,mi0_0,fa0_5,sol0_5,la0_5,si0_0,do1_5,
-                re1_5,mi1_0,fa1_5,sol1_5,la1_5,si1_0,do2_5,re2_5};
-        scaleArraySharp = new int[]{do0_5,re0_5,fa0_0,fa0_5,sol0_5,la0_5,do1_0,
-                do1_5,re1_5,fa1_0,fa1_5,sol1_5,la1_5,do2_0,do2_5,re2_5,mi2_5};
 
     }
 }
