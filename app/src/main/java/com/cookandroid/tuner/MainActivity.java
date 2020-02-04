fca034a786e0a7999ef38c1dc5cc2c7b78a8188c
jjun990908 <qkrwnsdud@kookmin.ac.kr>
2020-02-04 14:28:34 +0900

    라벨 회전 함수 추가

--- app/src/main/java/com/cookandroid/tuner/MainActivity.java
+++ app/src/main/java/com/cookandroid/tuner/MainActivity.java
@@ -64,8 +64,39 @@ public class MainActivity extends AppCompatActivity{
     public void setFlatText(){for(int i=0; i<17;i++){buttonArray[i].setText(ScaleArray[i%7]+"♭");}}
     //건반라벨 원음변환함수
     public void setOriginText(){ for(int i=0; i<17;i++){buttonArray[i].setText(ScaleArray[i%7]);} }
-    //튜닝확인 뷰 색 변환 함수
-
+    //음계 원판 돌림 함수
+    public void rotateLabel(float InputAudioHz){
+        if(InputAudioHz>=261 && InputAudioHz<277){
+            sound_label.setRotation((277- InputAudioHz)*(float)-1.875);
+        }
+        else if (InputAudioHz>=277 && InputAudioHz<293){
+            sound_label.setRotation((293- InputAudioHz)*(float)-1.875 -30);
+        }
+        else if (InputAudioHz>=293 && InputAudioHz<311){
+            sound_label.setRotation((311- InputAudioHz)*(float)-1.875 - 60);
+        }
+        else if (InputAudioHz>=311 && InputAudioHz<329){
+            sound_label.setRotation((329- InputAudioHz)*(float)-1.875 - 90);
+        }
+        else if (InputAudioHz>=329 && InputAudioHz<349){
+            sound_label.setRotation((349- InputAudioHz)*(float)-1.5 - 120);
+        }
+        else if (InputAudioHz>=349 && InputAudioHz<370){
+            sound_label.setRotation((370- InputAudioHz)*(float)-1.428 - 150);
+        }
+        else if (InputAudioHz>=370 && InputAudioHz<392){
+            sound_label.setRotation((370- InputAudioHz)*(float)-1.363 - 180);
+        }
+        else if (InputAudioHz>=392 && InputAudioHz<415){
+            sound_label.setRotation((370- InputAudioHz)*(float)-1.304 -210);
+        }
+        else if (InputAudioHz>=415 && InputAudioHz<440){
+            sound_label.setRotation((370- InputAudioHz)*(float)-1.2 - 240);
+        }
+        else if (InputAudioHz>=440 && InputAudioHz<466){
+            sound_label.setRotation((370- InputAudioHz)*(float)-1.25 - 270);
+        }
+    }
 
 
     @SuppressLint("ClickableViewAccessibility")
@@ -689,39 +720,7 @@ public class MainActivity extends AppCompatActivity{
 
             Log.i("current Hz", Float.toString(InputAudioHz));
             if(InputAudioHz>100) {  DebugHz(InputAudioHz);  }
-            if(InputAudioHz>=261 && InputAudioHz<277){
-                sound_label.setRotation((277- InputAudioHz)*(float)-1.875);
-            }
-            else if (InputAudioHz>=277 && InputAudioHz<293){
-                sound_label.setRotation((293- InputAudioHz)*(float)-1.875 -30);
-            }
-            else if (InputAudioHz>=293 && InputAudioHz<311){
-                sound_label.setRotation((311- InputAudioHz)*(float)-1.875 - 60);
-            }
-            else if (InputAudioHz>=311 && InputAudioHz<329){
-                sound_label.setRotation((329- InputAudioHz)*(float)-1.875 - 90);
-            }
-            else if (InputAudioHz>=329 && InputAudioHz<349){
-                sound_label.setRotation((349- InputAudioHz)*(float)-1.5 - 120);
-            }
-            else if (InputAudioHz>=349 && InputAudioHz<370){
-                sound_label.setRotation((370- InputAudioHz)*(float)-1.428 - 150);
-            }
-            else if (InputAudioHz>=370 && InputAudioHz<392){
-                sound_label.setRotation((370- InputAudioHz)*(float)-1.363 - 180);
-            }
-            else if (InputAudioHz>=392 && InputAudioHz<415){
-                sound_label.setRotation((370- InputAudioHz)*(float)-1.304 -210);
-            }
-            else if (InputAudioHz>=415 && InputAudioHz<440){
-                sound_label.setRotation((370- InputAudioHz)*(float)-1.2 - 240);
-            }
-            else if (InputAudioHz>=440 && InputAudioHz<466){
-                sound_label.setRotation((370- InputAudioHz)*(float)-1.25 - 270);
-            }
-
-
-
+            rotateLabel(InputAudioHz);
         }
     }
 }
