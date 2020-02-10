8004a0b7f4edf7da0d160955847d2547a4c661e4
kaspee <kaspee@naver.com>
2020-02-10 14:16:03 +0900

    시작시 권한 강화 - 재수정

--- app/src/main/java/com/cookandroid/tuner/SplashActivity.java
+++ app/src/main/java/com/cookandroid/tuner/SplashActivity.java
@@ -20,31 +20,34 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // Delay before initialize MainActivity.java
 
-        final TimerTask Auth = new TimerTask() {
-            @Override
-            public void run() {
-                Log.i("Waiting", "계속 도는 중");
-                if(ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED){
-                    ActivityCompat.requestPermissions(SplashActivity.this, new String[]{Manifest.permission.RECORD_AUDIO},0);
-                    Log.i("wating response", ":   initialize");
-                }
-                else{
-                    cancel();
-                    Intent intent = new Intent(SplashActivity.this, MainActivity.class);
-
-                    startActivity(intent);
-                    finish();
-
-                }
-
-
-            }
-        };
-
-        final Timer timer = new Timer();
-        timer.schedule(Auth,0,5000);
+        new Timer().schedule(Auth,3000,5000); // 아래에 있는 Auth(오디오 권한 받기) 를 5초마다 실행함.
 
     }
+
+
+
+
+
+
+
+    final TimerTask Auth = new TimerTask() {
+        @Override
+        public void run() {
+            Log.i("사용자의 마이크 권한을 받는 중...", "");
+            if(ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED){
+                ActivityCompat.requestPermissions(SplashActivity.this, new String[]{Manifest.permission.RECORD_AUDIO},0);
+                Log.i("Wating response", ":         AUDIO");
+            }
+            else{
+                cancel(); // 지금 이 Task 를 멈춘다.
+
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }
+    };
+
 }
+
