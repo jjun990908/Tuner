2dbc35b77175ce3f8d55885a1de365fd56dff025
kaspee <kaspee@naver.com>
2020-02-10 14:03:18 +0900

    시작시 권한 강화

--- app/src/main/java/com/cookandroid/tuner/SplashActivity.java
+++ app/src/main/java/com/cookandroid/tuner/SplashActivity.java
@@ -7,6 +7,7 @@ import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,30 +21,30 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         // Delay before initialize MainActivity.java
-        try{ Thread.sleep(2300);}
-        catch (InterruptedException e) {e.printStackTrace();}
 
         final TimerTask Auth = new TimerTask() {
             @Override
             public void run() {
+                Log.i("Waiting", "계속 도는 중");
                 if(ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED){
                     ActivityCompat.requestPermissions(SplashActivity.this, new String[]{Manifest.permission.RECORD_AUDIO},0);
+                    Log.i("wating response", ":   initialize");
                 }
                 else{
+                    cancel();
                     Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+
                     startActivity(intent);
                     finish();
+
                 }
+
+
             }
         };
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
-        finish();
 
-/*
         final Timer timer = new Timer();
-        timer.schedule(Auth,0,3000);
-*/
+        timer.schedule(Auth,0,5000);
 
     }
 }
