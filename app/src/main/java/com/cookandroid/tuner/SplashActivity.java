30d71f8de5836a6b3b69ec8be40813157f233feb
kaspee <kaspee@naver.com>
2020-02-10 11:31:19 +0900

    권한 얻기 수정

--- app/src/main/java/com/cookandroid/tuner/SplashActivity.java
+++ app/src/main/java/com/cookandroid/tuner/SplashActivity.java
@@ -6,51 +6,41 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.Handler;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // Delay before initialize MainActivity.java
+        try{ Thread.sleep(2300);}
+        catch (InterruptedException e) {e.printStackTrace();}
 
-        SharedPreferences pref = getSharedPreferences("checkFirst", Activity.MODE_PRIVATE);
-        boolean checkFirst = pref.getBoolean("checkFirst",false);
-        if(!checkFirst){
-            Toast.makeText(SplashActivity.this, "튜닝 기능을 위해 마이크 권한을 허가해주세요.", Toast.LENGTH_SHORT).show();
-            SharedPreferences.Editor editor = pref.edit();
-            editor.putBoolean("checkFirst",true);
-            editor.commit();
-            try{
-            Thread.sleep(2300);}
-            catch (InterruptedException e){
-                e.printStackTrace();
-            }
-        }
-
-
-        while (true) {
-            if (ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
-                //마이크 권한 확인
-                ActivityCompat.requestPermissions(SplashActivity.this, new String[]{Manifest.permission.RECORD_AUDIO}, 0);
-                //없다면 마이크 권한 요청
-            } else {
-                try {
-                    Thread.sleep(2300);
-
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
+        final TimerTask Auth = new TimerTask() {
+            @Override
+            public void run() {
+                if(ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED){
+                    ActivityCompat.requestPermissions(SplashActivity.this, new String[]{Manifest.permission.RECORD_AUDIO},0);
                 }
-
-                Intent intent = new Intent(this, MainActivity.class);
-                startActivity(intent);
-                finish();
-                break;
-
+                else{
+                    Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
             }
-        }
+        };
+
+
+
+        final Timer timer = new Timer();
+        timer.schedule(Auth,0,3000);
 
 
     }
