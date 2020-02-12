432af55bc626074cb546d612d3224fd6b44f6402
kaspee <kaspee@naver.com>
2020-02-12 15:16:55 +0900

    네비게이션바 숨기기

--- app/src/main/java/com/cookandroid/tuner/SplashActivity.java
+++ app/src/main/java/com/cookandroid/tuner/SplashActivity.java
@@ -10,6 +10,7 @@ import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
+import android.view.View;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -27,7 +28,14 @@ public class SplashActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         new Timer().schedule(Auth,3000,5000); // 아래에 있는 Auth(오디오 권한 받기) 를 5초마다 실행함.
-
+        View decorView = getWindow().getDecorView();
+        // Hide both the navigation bar and the status bar.
+        // SYSTEM_UI_FLAG_FULLSCREEN is only available on Android 4.1 and higher, but as
+        // a general rule, you should design your app to hide the status bar whenever you
+        // hide the navigation bar.
+        int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
     }
 
 
