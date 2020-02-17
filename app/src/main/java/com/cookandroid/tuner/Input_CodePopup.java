9554f77e20b5b6889d6a230556d00e0e6127351c
LeeSeonyong <birdskystar@kookmin.ac.kr>
2020-02-17 15:55:38 +0900

    베타 버전

--- app/src/main/java/com/cookandroid/tuner/Input_CodePopup.java
+++ app/src/main/java/com/cookandroid/tuner/Input_CodePopup.java
@@ -1,7 +1,6 @@
 package com.cookandroid.tuner;
 
-import android.annotation.SuppressLint;
-import android.content.DialogInterface;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
@@ -12,7 +11,6 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.anjlab.android.iab.v3.BillingProcessor;
@@ -24,7 +22,10 @@ public class Input_CodePopup extends AppCompatActivity implements BillingProcess
     TextView txt_codeview, text_Explanation2;
     Button btn_close,btn_confirmcode, btn_confirm, btn_buycode;
     boolean codenumbercheck =false , codeenglishcheck =false;
-    private BillingProcessor bp, mBillingProcessor;
+    Context mContext;
+    BillingProcessor bp, mBillingProcessor;
+    //private PurchaseHeartsAdapter skusAdapter;
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         // onActivityResult 부분이 없을시 구글 인앱 결제창이 동시에 2개가 나타나는 현상이 발생하였고 (원인은 잘 모름)
@@ -44,33 +45,31 @@ public class Input_CodePopup extends AppCompatActivity implements BillingProcess
         // SkuDetails.priceText: 아이템 가격에 현지 화폐 단위를 붙인 String을 리턴한다. 예를들면 '0.99$'이런 식이다.
         // SkuDetails.priceLong: 가격을 long 으로 리턴한다. 0.99 이런식이다.
         // SkuDetails.productId: 제품ID를 가지고 온다. 이를 통해서 어떤 아이템을 구매했는지 판별 가능하다.
-        SkuDetails mProduct = mBillingProcessor.getPurchaseListingDetails("buycode");
+        SkuDetails mProduct = mBillingProcessor.getPurchaseListingDetails("buyplaymode");
         if(mProduct != null) {
             String temp = mProduct.productId + " / " + mProduct.priceText + " / "
                     + mProduct.priceValue + " / " + mProduct.priceLong;
             Toast.makeText(this, temp, Toast.LENGTH_SHORT).show();
+        } else {
         }
     }
+
     @Override
     public void onProductPurchased(String productId, TransactionDetails details) {
-        /*
-         * Called when requested PRODUCT ID was successfully purchased
-         */
+        SkuDetails skuDetails = mBillingProcessor.getPurchaseListingDetails("buyplaymode");
 
-        // 구매 성공시 호출되는 메소드이다. 구매 성공 이후 수행해야 되는 작업을 수행하면 된다.
-        // 예시는 구매 성공 다이얼로그를 실행하는 것이다.
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setMessage("구매 성공 하였습니다. 코드 'PM18602'를 입력해주세요")
-                .setCancelable(false)
-                .setPositiveButton("확인", new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int id) {
-                        // to do action
-                    }
-                });
-        AlertDialog alert = builder.create();
-        alert.show();
+        SharedPreferences sharedPreferences = getSharedPreferences("check", MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        Boolean checked = true;
+        editor.putBoolean("codecheck", checked);
+        editor.commit();
+
+        SharedPreferences cc = getSharedPreferences("check", MODE_PRIVATE);
+
+        startActivity(new Intent(getApplicationContext(), play_mode.class));
+        overridePendingTransition(R.anim.anim_slide_down, R.anim.anim_slide_up);
+        finish();
     }
-
     @Override
     public void onBillingError(int errorCode, Throwable error) {
         /*
@@ -87,7 +86,6 @@ public class Input_CodePopup extends AppCompatActivity implements BillingProcess
             Toast.makeText(this, errorMessage, Toast.LENGTH_SHORT).show();
         }
     }
-
     @Override
     public void onPurchaseHistoryRestored() {
         /*
@@ -95,18 +93,13 @@ public class Input_CodePopup extends AppCompatActivity implements BillingProcess
          * was loaded from Google Play
          */
     }
-
-
-
-
-    @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.input_code_popup);
-        bp = new BillingProcessor(this,"MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAiCjoveQ2+WNj5QElXLUDsUAz5edl+bVJjZ62p3Ig1u2J/nfN6nPE4Cwp4cEIOue7ClVRRQM3sObXHnO+1nUYEbPXnCpNTdx0+Yll6A1eqKJFI6LaSArtKZj7kHj7fIBZkzeJB8yqpe6N+kVI2igQ4ZkK2SwVWjBPBi6+SmHvPIluZ4LmGudVvJeRQ8dooe0y6To6x8HLcaqJPluKvAWYC8vNDXFhKwBdEtfbQJbsUN/k2TI4uReH2gXi1vHKnIHZ3hQz1GL+/CAluNmEzRP5CHOiwCdxVz9MYCk0iDRIuo06o88sFPSJrInW0hSulZsQENdiWpnKhaq5IZdaZTtdxwIDAQAB",this);
+        mBillingProcessor = new BillingProcessor(this,"MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAiCjoveQ2+WNj5QElXLUDsUAz5edl+bVJjZ62p3Ig1u2J/nfN6nPE4Cwp4cEIOue7ClVRRQM3sObXHnO+1nUYEbPXnCpNTdx0+Yll6A1eqKJFI6LaSArtKZj7kHj7fIBZkzeJB8yqpe6N+kVI2igQ4ZkK2SwVWjBPBi6+SmHvPIluZ4LmGudVvJeRQ8dooe0y6To6x8HLcaqJPluKvAWYC8vNDXFhKwBdEtfbQJbsUN/k2TI4uReH2gXi1vHKnIHZ3hQz1GL+/CAluNmEzRP5CHOiwCdxVz9MYCk0iDRIuo06o88sFPSJrInW0hSulZsQENdiWpnKhaq5IZdaZTtdxwIDAQAB" ,this);
+        bp = new BillingProcessor(this, "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAiCjoveQ2+WNj5QElXLUDsUAz5edl+bVJjZ62p3Ig1u2J/nfN6nPE4Cwp4cEIOue7ClVRRQM3sObXHnO+1nUYEbPXnCpNTdx0+Yll6A1eqKJFI6LaSArtKZj7kHj7fIBZkzeJB8yqpe6N+kVI2igQ4ZkK2SwVWjBPBi6+SmHvPIluZ4LmGudVvJeRQ8dooe0y6To6x8HLcaqJPluKvAWYC8vNDXFhKwBdEtfbQJbsUN/k2TI4uReH2gXi1vHKnIHZ3hQz1GL+/CAluNmEzRP5CHOiwCdxVz9MYCk0iDRIuo06o88sFPSJrInW0hSulZsQENdiWpnKhaq5IZdaZTtdxwIDAQAB",this);
         bp.initialize();
-
         btn_confirm = findViewById(R.id.btn_confirm);
         btn_confirm.setOnClickListener(new View.OnClickListener(){
             @Override
@@ -167,14 +160,16 @@ public class Input_CodePopup extends AppCompatActivity implements BillingProcess
 
         });
         btn_buycode = findViewById(R.id.btn_buycode);
-       // btn_buycode.setOnClickListener(new View.OnClickListener() {
-           // @Override
-           // public void onClick(View v) {
-                  //  bp.purchase(Input_CodePopup.this,"buycode");
-          //  }
-      //  });
+        btn_buycode.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mBillingProcessor.purchase(Input_CodePopup.this,"buyplaymode");
+            }
+        });
 
         btn_close = (Button)findViewById(R.id.btn_close);
+
+
 
 
         //닫기버튼 클릭함수
@@ -188,11 +183,7 @@ public class Input_CodePopup extends AppCompatActivity implements BillingProcess
                 return false;
             }
         });
-//        btn_close.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                finish();
-//            }
-//        });
     }
+
+
 }
